d09d21cf0b12bdef178607e23f067f65b4f21a07
Megan Lee <melee124@gmail.com>
2018-04-04 17:16:42 -0700

    redid HTML.  Inputs are now working, but only the first if statement works for each function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 //biz logic
 // Check if the user is ready to play!
 
-function stairsOrDoor() {
+function enterOrWuss () {
     var response = document.getElementById("response").value;
 
-    if (response === 'ENTER MAZE') {
+    if (response === 'ENTER MAZE HOUSE') {
       document.getElementById("enter").classList.toggle('hidden');
       document.getElementById("story").classList.toggle('hidden');
     } else if (response == 'WUSS OUT') {
@@ -25,28 +25,30 @@ function weaponofChoice() {
   } else {
     document.getElementById("basement").classList.toggle('hidden');
     document.getElementById("story").classList.toggle('hidden');
-}
+  }
+};
 
 //UL Logic
-$( document ).ready(function() {
-  showSub();
-  let charName = $("#cname"), sub = $("#subtitle"), charID = $("#idLine");
+//$( document ).ready(function() {
+  //showSub();
+  //let charName = $("#cname"), sub = $("#subtitle"), charID = $("#idLine");
 
-  function showSub () {
-    setTimeout(function(){
-      sub.show();
-    }, 1500);
-  }
+//  function showSub () {
+  //  setTimeout(function(){
+    //  sub.show();
+  //  }, 1500);
+//  }
+//}
 
-  $("#nbtn").on("click", function(){
-    event.preventDefault();
-    if (charName.val() == ""){
-      $("#nmform").append("<p>You cannot enter the wilds without a name. Please tell us what to call you.</p>").append("<button id= 'ok'>Try again.</button");
-    } else {
-      charID.append("<span>You are " + charName.val() +"</span>");
-      $("#nmform").addClass("hide");
-      $("#raceform").removeClass("hide");
-    }
-  });
-  });
-});
+  //$("#nbtn").on("click", function(){
+    //event.preventDefault();
+    //if (charName.val() == ""){
+    //  $("#nmform").append("<p>You cannot enter the wilds without a name. Please tell us what to call you.</p>").append("<button id= 'ok'>Try again.</button");
+    //} else {
+      //charID.append("<span>You are " + charName.val() +"</span>");
+    //  $("#nmform").addClass("hide");
+    //  $("#raceform").removeClass("hide");
+  //  }
+  //});
+  //});
+//});
